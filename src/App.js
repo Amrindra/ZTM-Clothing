6a110a77c3d243126737b5ca1f7644ada0f374b3
Amrindra Ouk <amrindraouk@yahoo.com>
2022-11-10 19:59:55 -0500

    Created Navigation component and add outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import Navigation from "./routes/navigation/Navigation";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
