fbbc40ddb264e2f0f6fdeb5a695ee2f5abbec3d0
Amrindra Ouk <amrindraouk@yahoo.com>
2022-11-15 01:36:47 -0500

    Migrating from sass to styled component

--- src/components/cartIcon/CartIcon.jsx
+++ src/components/cartIcon/CartIcon.jsx
@@ -1,18 +1,15 @@
-import "./CartIcon.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cartContext";
+import { CartIconContainer, ItemCount } from "./CartIconStyle";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartQtyCount } = useContext(CartContext);
   return (
-    <div
-      className="cart-icon-container"
-      onClick={() => setIsCartOpen(!isCartOpen)}
-    >
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartQtyCount}</span>
-    </div>
+    <CartIconContainer onClick={() => setIsCartOpen(!isCartOpen)}>
+      <ShoppingIcon />
+      <ItemCount>{cartQtyCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
--- src/components/directoryItem/DirectoryItem.jsx
+++ src/components/directoryItem/DirectoryItem.jsx
@@ -1,21 +1,47 @@
-import "./DirectoryItem.scss";
+import { useNavigate } from "react-router-dom";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./DirectoryItemStyle";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="directory-item-body">
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
 export default DirectoryItem;
+
+// import "./DirectoryItem.scss";
+
+// const DirectoryItem = ({ category }) => {
+//   const { imageUrl, title } = category;
+//   return (
+//     <div className="directory-item-container">
+//       <div
+//         className="background-image"
+//         style={{
+//           backgroundImage: `url(${imageUrl})`,
+//         }}
+//       />
+//       <div className="directory-item-body">
+//         <h2>{title}</h2>
+//         <p>Shop Now</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DirectoryItem;
