e99a13a5a4296aed2059f9441062dfb5a4cc30b8
Amrindra Ouk <amrindraouk@yahoo.com>
2022-11-28 01:26:08 -0500

    Switched redux to use redux with typescript

---
+++ src/store/categories/categoryAction.ts
@@ -0,0 +1,31 @@
+import { Action, ActionWithPayload, createAction, withMatcher } from "../../utils/reducer/reducer";
+import { CATEGORIES_ACTION_TYPES, Category } from "./categoryType";
+
+export type FetchCategoriesStart =
+  Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+
+export type FetchCategoriesSuccess = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+  Category[]
+>;
+
+export type FetchCategoriesFailed = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+  Error
+>;
+
+export const fetchCategoriesStart = withMatcher(() =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+);
+
+export const fetchCategoriesSuccess = withMatcher(
+  (categoriesArray: Category[]) =>
+    createAction(
+      CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+      categoriesArray
+    )
+);
+
+export const fetchCategoriesFailed = withMatcher((error: Error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+);
