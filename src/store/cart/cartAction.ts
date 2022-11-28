16303ae47e0f4a6ce6cf1a632ad5b6fb3d268340
Amrindra Ouk <amrindraouk@yahoo.com>
2022-11-28 01:31:58 -0500

    Implemented the cart store to use typescript

---
+++ src/store/cart/cartAction.ts
@@ -0,0 +1,155 @@
+import { ActionWithPayload, createAction, withMatcher } from "../../utils/reducer/reducer";
+import { CategoryItem } from "../categories/categoryType";
+import { CartItem, CART_ACTION_TYPES } from "./cartType";
+
+const addCartItem = (
+  cartItems: CartItem[],
+  productToAdd: CategoryItem
+): CartItem[] => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const removeCartItem = (
+  cartItems: CartItem[],
+  cartItemToRemove: CategoryItem
+): CartItem[] => {
+  // find the cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  // check if quantity is equal to 1, if it is remove that item from the cart
+  if (existingCartItem && existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // return back cartitems with matching cart item with reduced quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+const clearCartItem = (cartItems: CartItem[], cartItemToClear: CartItem) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+export type SetCartIsOpen = ActionWithPayload<
+  CART_ACTION_TYPES.SET_IS_CART_OPEN,
+  boolean
+>;
+
+export type SetCartItems = ActionWithPayload<
+  CART_ACTION_TYPES.SET_CART_ITEMS,
+  CartItem[]
+>;
+
+export const setIsCartOpen = withMatcher((boolean: boolean) =>
+  createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean)
+);
+
+export const setCartItems = withMatcher((cartItems: CartItem[]) =>
+  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cartItems)
+);
+
+export const addItemToCart = (
+  cartItems: CartItem[],
+  productToAdd: CategoryItem
+) => {
+  const newCartItems = addCartItem(cartItems, productToAdd);
+  return setCartItems(newCartItems);
+};
+
+export const removeItemFromCart = (
+  cartItems: CartItem[],
+  cartItemToRemove: CartItem
+) => {
+  const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+  return setCartItems(newCartItems);
+};
+
+export const clearItemFromCart = (
+  cartItems: CartItem[],
+  cartItemToClear: CartItem
+) => {
+  const newCartItems = clearCartItem(cartItems, cartItemToClear);
+  return setCartItems(newCartItems);
+};
+
+
+
+
+// import { createAction } from "../../utils/reducer/reducer";
+// import { CART_ACTION_TYPES } from "./cartType";
+
+// const addCartItem = (cartItems, productToAdd) => {
+//   const existingCartItem = cartItems.find(
+//     (cartItem) => cartItem.id === productToAdd.id
+//   );
+
+//   if (existingCartItem) {
+//     return cartItems.map((cartItem) =>
+//       cartItem.id === productToAdd.id
+//         ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//         : cartItem
+//     );
+//   }
+
+//   // ...cartItems here meaning that to spread all the exisiting items
+//   //{ ...productToAdd, quantity: 1 } meaning that to add new item when user add to cart and add qty as well
+//   return [...cartItems, { ...productToAdd, quantity: 1 }];
+// };
+
+// const removeCartItem = (cartItems, cartItemToRemove) => {
+//   // find the cart item to remove
+//   const existingCartItem = cartItems.find(
+//     (cartItem) => cartItem.id === cartItemToRemove.id
+//   );
+
+//   // check if quantity is equal to 1, if it is remove that item from the cart
+//   if (existingCartItem.quantity === 1) {
+//     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+//   }
+
+//   // return back cartitems with matching cart item with reduced quantity
+//   return cartItems.map((cartItem) =>
+//     cartItem.id === cartItemToRemove.id
+//       ? { ...cartItem, quantity: cartItem.quantity - 1 }
+//       : cartItem
+//   );
+// };
+
+// const clearCartItem = (cartItems, cartItemToClear) =>
+//   cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+// export const addItemToCart = (cartItems, productToAdd) => {
+//   const newCartItems = addCartItem(cartItems, productToAdd);
+//   return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+// };
+
+// export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+//   const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+//   return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+// };
+
+// export const clearItemFromCart = (cartItems, cartItemToClear) => {
+//   const newCartItems = clearCartItem(cartItems, cartItemToClear);
+//   return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+// };
+
+// export const setIsCartOpen = (boolean) =>
+//   createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean);
+
+
