cdda332e99e1c773c1ce661567efabe18daf9242
Amrindra Ouk <amrindraouk@yahoo.com>
2022-12-07 18:00:18 -0500

    fixed bug

--- src/utils/firebase/firebase.ts
+++ src/utils/firebase/firebase.ts
@@ -25,12 +25,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "ztm-clothing-db-13ee1.firebaseapp.com",
+  projectId: "ztm-clothing-db-13ee1",
+  storageBucket: "ztm-clothing-db-13ee1.appspot.com",
+  messagingSenderId: "540926130827",
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -42,8 +42,10 @@ googleProvider.setCustomParameters({
 });
 
 export const auth = getAuth();
+
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
+
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
